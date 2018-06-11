f825bd08d9a3b77938f6a13e2a8594b9bb46b299
Hugo Alliaume <kocal@live.fr>
2018-06-11 21:34:40 +0200

    test: add tests :)

---
+++ test/index.test.js
@@ -0,0 +1,58 @@
+const Vue = require('vue');
+const { createLocalVue, mount } = require('@vue/test-utils');
+const VueNumerals = require('../src');
+
+Vue.config.productionTip = false;
+
+describe('filter', () => {
+  let localVue;
+  let wrapper;
+
+  beforeEach(() => {
+    localVue = createLocalVue();
+  });
+
+  test('format with default locale and default format', () => {
+    localVue.use(VueNumerals);
+
+    wrapper = mount({
+      template: '<p>{{ 12345 | numeralFormat }}</p>',
+    }, { localVue });
+
+    expect(wrapper.html()).toBe('<p>12,345</p>');
+  });
+
+  test('format with default locale and custom format', () => {
+    localVue.use(VueNumerals);
+
+    wrapper = mount({
+      template: '<p>{{ 12345 | numeralFormat("0o") }}</p>',
+    }, { localVue });
+
+    expect(wrapper.html()).toBe('<p>12345th</p>');
+  });
+
+  test('format with custom locale and default format', () => {
+    localVue.use(VueNumerals, {
+      locale: 'en',
+    });
+
+    wrapper = mount({
+      template: '<p>{{ 12345 | numeralFormat }}</p>',
+    }, { localVue });
+
+    expect(wrapper.html()).toBe('<p>12,345</p>');
+  });
+
+  test('format with custom locale and custom format', () => {
+    localVue.use(VueNumerals, {
+      locale: 'fr'
+    });
+
+    wrapper = mount({
+      template: '<p>{{ 12345 | numeralFormat("0,0[.]00 $") }}</p>',
+    }, { localVue });
+
+    expect(wrapper.html()).toBe('<p>12 345 €</p>');
+  });
+});
