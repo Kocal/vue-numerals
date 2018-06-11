b2ffc600f69eb5b3f3aae3a6c3859766e2a2a555
Hugo Alliaume <kocal@live.fr>
2018-06-11 21:34:58 +0200

    feat: implement filter

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 'use strict';
 
-function install(Vue) {
-  console.log(Vue);
+const numeral = require('numeral');
+const locales = require('numeral/locales');
+
+/**
+ * @param {VueConstructor} Vue
+ * @param {Object}  config
+ * @param {string?} config.locale
+ */
+function install(Vue, config) {
+  config = config || {};
+  const locale = config.locale || (navigator.language || navigator.languages[0] || 'fr').toLowerCase().substr(0, 2);
+
+  numeral.locale(locale);
+
+  Vue.filter('numeralFormat', function (value, format = '0,0') {
+    return numeral(value).format(format);
+  });
 }
 
 if (typeof window !== 'undefined' && window.Vue) {
