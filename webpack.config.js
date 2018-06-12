6580700473915fc1374b911b65fd079580f3a371
Hugo Alliaume <kocal@live.fr>
2018-06-12 07:01:30 +0200

    chore: configure webpack to use babel-loader

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const config = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
+};
+
+module.exports = config;
