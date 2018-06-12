d9d3cea6cbcaf902d3ef235a41cd6649ffb43cc9
Hugo Alliaume <kocal@live.fr>
2018-06-12 21:26:36 +0200

    chore: fix lint errors

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-'use strict';
-
 import numeral from 'numeral';
 import 'numeral/locales';
 
@@ -7,15 +5,16 @@ import 'numeral/locales';
  * @param {VueConstructor} Vue
  * @param {string?} locale
  */
-function install(Vue, { locale } = {}) {
-  locale = (locale || navigator.language || navigator.languages[0] || 'fr').toLowerCase().substr(0, 2);
+function install(Vue, { locale }) {
+  const numeralLocale = (locale || navigator.language || navigator.languages[0] || 'fr').toLowerCase().substr(0, 2);
 
-  numeral.locale(locale);
+  numeral.locale(numeralLocale);
 
   Vue.filter('numeralFormat', (value, format = '0,0') => numeral(value).format(format));
 }
 
 if (typeof window !== 'undefined' && window.Vue) {
+  // eslint-disable-next-line no-undef
   Vue.use(install);
 }
 
