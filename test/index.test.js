9d749553eff8306a04f043a3592a35a899baf308
Hugo Alliaume <kocal@live.fr>
2019-04-07 07:57:20 +0200

    fix tests

--- test/index.test.js
+++ test/index.test.js
@@ -12,8 +12,24 @@ describe('filter', () => {
     localVue = createLocalVue();
   });
 
-  test('format with default locale (fr) and default format', () => {
+  test('format with default locale (en) and default format', () => {
     localVue.use(VueNumerals);
+
+    wrapper = mount({ template: '<p>{{ 12345 | numeralFormat }}</p>' }, { localVue });
+
+    expect(wrapper.html()).toBe('<p>12,345</p>');
+  });
+
+  test('format with default locale (en) and custom format', () => {
+    localVue.use(VueNumerals);
+
+    wrapper = mount({ template: '<p>{{ 12345 | numeralFormat("0o") }}</p>' }, { localVue });
+
+    expect(wrapper.html()).toBe('<p>12345th</p>');
+  });
+
+  test('format with custom locale (fr) and default format', () => {
+    localVue.use(VueNumerals, { locale: 'fr' });
 
     wrapper = mount({ template: '<p>{{ 12345 | numeralFormat }}</p>' }, { localVue });
 
@@ -21,19 +37,11 @@ describe('filter', () => {
   });
 
   test('format with default locale (fr) and custom format', () => {
-    localVue.use(VueNumerals);
+    localVue.use(VueNumerals, { locale: 'fr' });
 
     wrapper = mount({ template: '<p>{{ 12345 | numeralFormat("0o") }}</p>' }, { localVue });
 
     expect(wrapper.html()).toBe('<p>12345e</p>');
-  });
-
-  test('format with custom locale (en) and default format', () => {
-    localVue.use(VueNumerals, { locale: 'en' });
-
-    wrapper = mount({ template: '<p>{{ 12345 | numeralFormat }}</p>' }, { localVue });
-
-    expect(wrapper.html()).toBe('<p>12,345</p>');
   });
 
   test('format with custom locale (ru) and custom format', () => {
