4dc9f92bb98605f1dbd3f572ea6f1890f0f11b7e
Hugo Alliaume <kocal@live.fr>
2020-09-03 08:35:28 +0200

    feat: drop Vue 2 support for Vue 3 (#387)
    BREAKING CHANGE: This plugin is now only compatible with Vue 3.

--- test/index.test.js
+++ test/index.test.js
@@ -1,53 +1,68 @@
-import Vue from 'vue';
-import { createLocalVue, mount } from '@vue/test-utils';
+import { mount } from '@vue/test-utils';
 import VueNumerals from '../src';
 
-Vue.config.productionTip = false;
-
 describe('filter', () => {
-  let localVue;
-  let wrapper;
-
-  beforeEach(() => {
-    localVue = createLocalVue();
-  });
-
   test('format with default locale (en) and default format', () => {
-    localVue.use(VueNumerals);
-
-    wrapper = mount({ template: '<p>{{ 12345 | numeralFormat }}</p>' }, { localVue });
+    const wrapper = mount(
+      { template: '<p>{{ numeralFormat(12345) }}</p>' },
+      {
+        global: {
+          plugins: [VueNumerals]
+        }
+      }
+    );
 
     expect(wrapper.html()).toBe('<p>12,345</p>');
   });
 
   test('format with default locale (en) and custom format', () => {
-    localVue.use(VueNumerals);
-
-    wrapper = mount({ template: '<p>{{ 12345 | numeralFormat("0o") }}</p>' }, { localVue });
+    const wrapper = mount(
+      { template: '<p>{{ numeralFormat(12345, "0o") }}</p>' },
+      {
+        global: {
+          plugins: [VueNumerals]
+        }
+      }
+    );
 
     expect(wrapper.html()).toBe('<p>12345th</p>');
   });
 
   test('format with custom locale (fr) and default format', () => {
-    localVue.use(VueNumerals, { locale: 'fr' });
-
-    wrapper = mount({ template: '<p>{{ 12345 | numeralFormat }}</p>' }, { localVue });
+    const wrapper = mount(
+      { template: '<p>{{ numeralFormat(12345) }}</p>' },
+      {
+        global: {
+          plugins: [[VueNumerals, { locale: 'fr' }]]
+        }
+      }
+    );
 
     expect(wrapper.html()).toBe('<p>12 345</p>');
   });
 
   test('format with default locale (fr) and custom format', () => {
-    localVue.use(VueNumerals, { locale: 'fr' });
-
-    wrapper = mount({ template: '<p>{{ 12345 | numeralFormat("0o") }}</p>' }, { localVue });
+    const wrapper = mount(
+      { template: '<p>{{ numeralFormat(12345, "0o") }}</p>' },
+      {
+        global: {
+          plugins: [[VueNumerals, { locale: 'fr' }]]
+        }
+      }
+    );
 
     expect(wrapper.html()).toBe('<p>12345e</p>');
   });
 
   test('format with custom locale (ru) and custom format', () => {
-    localVue.use(VueNumerals, { locale: 'ru' });
-
-    wrapper = mount({ template: '<p>{{ 12345 | numeralFormat("0,0[.]00 $") }}</p>' }, { localVue });
+    const wrapper = mount(
+      { template: '<p>{{ numeralFormat(12345, "0,0[.]00 $") }}</p>' },
+      {
+        global: {
+          plugins: [[VueNumerals, { locale: 'ru' }]]
+        }
+      }
+    );
 
     expect(wrapper.html()).toBe('<p>12 345 руб.</p>');
   });
